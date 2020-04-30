da7ae17bd38346c28a26343b4d6e9620b89ec639
taka5963 <big-hill-filial-piety-333-@docomo.ne.jp>
2020-04-30 13:16:51 +0900

    【マークアップ 】ユーザー新規登録

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def new1
+  end
+
+  def new2
+  end
+  
+  def new3
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root 'users#new1'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "users", to: "users#new1"
+  resources :users do
+    collection do
+      get 'new1'
+      post 'new2'
+      # post 'step3'  #入力が全て完了
+      # get 'complete_signup'  #登録完了後
+    end
+  end
 end
