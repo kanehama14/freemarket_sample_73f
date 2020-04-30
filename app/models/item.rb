64041ba92660c7d1b1946ef5018fe54108ddfd01
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-05-01 00:43:22 +0900

    新規登録機能を実装
    新規登録の機能を実装。
    imageの複数登録は未実装。

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,11 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_fee
-  belongs_to_active_hash :prefecture
   belongs_to :delivery_day
   belongs_to :status
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
