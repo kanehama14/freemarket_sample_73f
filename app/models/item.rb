a19680bcc3672e0ae44cf8fa8a2e781f35122baf
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-04-30 10:19:42 +0900

    モデルファイルにアソシエーションを記述
    userモデル、addressモデル以外のモデルファイルにアソシエーションを記述。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  has_one :address
+  has_many :images
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to_active_hash :prefecture
+  belongs_to :delivery_days
+  belongs_to :status
+  belongs_to :user
 end
