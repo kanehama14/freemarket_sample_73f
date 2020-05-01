c1cbf4e7bf2ec644f17465cec06c4ffa1b588b78
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-05-01 13:52:28 +0900

    画像を複数登録できる機能を追加
    jsファイルとimages_attributesメソッドを使って画像を複数登録できるように修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,6 @@ class Item < ApplicationRecord
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
