16c3a779c678fb001d0a99dd678a956c983ef61a
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-05-02 00:18:47 +0900

    添付した画像が表示されるように実装
    添付したらイメージファイルが表示されるように実装

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -10,20 +10,30 @@ $(document).on('turbolinks:load', ()=> {
     return html;
   }
   // プレビュー用のimgタグを生成する関数
-  // const buildImg = (index, url)=> {
-  //   const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
-  //   return html;
-  // }
+  const buildImg = (index, url)=> {
+    const html = `<img data-index="${index}" src="${url}" class="tmpimg">`;
+    return html;
+  }
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
   console.log("test");
   $('#image-box').on('change', '.js-file', function(e) {
-    console.log("testtetstest");
-    // fileIndexの先頭の数字を使ってinputを作る
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    // 末尾の数に1足した数を追加する
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    const targetIndex = $(this).parent().data('index');
+    // ファイルのブラウザ上でのURLを取得する
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+
+    // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
+    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      img.setAttribute('src', blobUrl);
+    } else {  // 新規画像追加の処理
+      $('#previews').append(buildImg(targetIndex, blobUrl));
+      // fileIndexの先頭の数字を使ってinputを作る
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      // 末尾の数に1足した数を追加する
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+    }
   });
 
   $('#image-box').on('click', '.js-remove', function() {
