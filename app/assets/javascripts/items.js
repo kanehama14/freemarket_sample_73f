0adf17f24aa27d246c5522d337feffe9d5e2cbb5
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-05-02 00:19:47 +0900

    商品の説明の文字数カウントを実装
    商品の説明の文字数カウントをJSで実装

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -42,3 +42,37 @@ $(document).on('turbolinks:load', ()=> {
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 });
+
+
+
+
+// jquery書きはじめ
+$(function (){
+  // 処理（ページが読み込まれた時、フォームに残り何文字入力できるかを数えて表示する）
+
+  //フォームに入力されている文字数を数える
+  //\nは"改行"に変換して2文字にする。オプションフラグgで文字列の最後まで\nを探し変換する
+  var count = $(".js-text").text().replace(/\n/g, "改行").length;
+  //残りの入力できる文字数を計算
+  var now_count = 1000 - count;
+  //文字数がオーバーしていたら文字色を赤にする
+  if (count > 1000) {
+    $(".js-text-count").css("color","red");
+  }
+  //残りの入力できる文字数を表示
+  $(".js-text-count").text(now_count + "/1000");
+
+  $(".js-text").on("keyup", function() {
+    // 処理（キーボードを押した時、フォームに残り何文字入力できるかを数えて表示する）
+    //フォームのvalueの文字数を数える
+    var count = $(this).val().replace(/\n/g, "改行").length;
+    var now_count = 1000 - count;
+
+    if (count > 1000) {
+      $(".js-text-count").css("color","red");
+    } else {
+      $(".js-text-count").css("color","black");
+    }
+    $(".js-text-count").text( now_count + "/1000");
+  });
+});
