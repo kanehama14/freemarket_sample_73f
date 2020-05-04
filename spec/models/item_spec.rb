73b87b523a3898bfc735c051cf675cea26daf034
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-05-05 00:21:34 +0900

    単体テストの記述の続き
    単体テストのテストコードを記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -25,5 +25,42 @@ describe Item do
       expect(item.errors[:category_id]).to include("を入力してください")
     end
 
+
+    it "商品状態がない場合は登録できないこと" do
+      item = FactoryBot.build(:item, condition_id: nil)
+      item.valid?
+      expect(item.errors[:condition_id]).to include("を入力してください")
+    end
+
+    it "配送料がない場合は登録できないこと" do
+      item = FactoryBot.build(:item, delivery_fee_id: nil)
+      item.valid?
+      expect(item.errors[:delivery_fee_id]).to include("を入力してください")
+    end
+
+    it "都道府県がない場合は登録できないこと" do
+      item = FactoryBot.build(:item, prefecture_id: nil)
+      item.valid?
+      expect(item.errors[:prefecture_id]).to include("を入力してください")
+    end
+
+    it "都道府県がない場合は登録できないこと" do
+      item = FactoryBot.build(:item, prefecture_id: nil)
+      item.valid?
+      expect(item.errors[:prefecture_id]).to include("を入力してください")
+    end
+
+    it "配達日がない場合は登録できないこと" do
+      item = FactoryBot.build(:item, delivery_day_id: nil)
+      item.valid?
+      expect(item.errors[:delivery_day_id]).to include("を入力してください")
+    end
+
+    it "金額がない場合は登録できないこと" do
+      item = FactoryBot.build(:item, price: nil)
+      item.valid?
+      expect(item.errors[:price]).to include("を入力してください")
+    end
+
   end
 end
