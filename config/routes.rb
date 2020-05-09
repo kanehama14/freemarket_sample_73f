e3f58a5085530fc0a237ffc06fb58a71d68bc4a1
yukino0307 <62429496+yukino0307@users.noreply.github.com>
2020-05-09 09:16:14 +0900

    ユーザー新規登録実装のためビューを修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,16 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions',
   }
 
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+
   root 'items#index'
-  resources :users, only: [:new, :index, :edit, :update] do
+  resources :users, only: [:new, :index] do
     collection do
       get 'logout'
       get 'credit_regist'
