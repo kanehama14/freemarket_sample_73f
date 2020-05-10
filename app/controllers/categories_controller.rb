aa4cd3f9d29485f9d2ff40086967a41b49802183
kanehama14 <kanehama14pro@gmail.com>
2020-05-10 12:16:23 +0900

    Update categories_controller.rb
    set_parentsアクションを定義

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,20 @@
 class CategoriesController < ApplicationController
+  before_action :set_parents
 
   def index
-    @parents = Category.all.order("id ASC").limit(13)
   end
 
   def parent_list
-    @parents = Category.all.order("id ASC").limit(13)
   end
   
   def child_list
-    @parents = Category.all.order("id ASC").limit(13)
   end
 
   def grandchild_list
-    @parents = Category.all.order("id ASC").limit(13)
   end
 
+  private
+  def set_parents
+    @parents = Category.all.order("id ASC").limit(13)
+  end
 end
