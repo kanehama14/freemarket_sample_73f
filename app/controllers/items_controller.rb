6d975c9b89e4edf9647eaebc9a8dbbf3a3bad98f
taka5963 <big-hill-filial-piety-333-@docomo.ne.jp>
2020-05-10 09:33:18 +0900

    【サーバーサイド】商品詳細表示４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -77,7 +77,8 @@ class ItemsController < ApplicationController
     )
     .merge(
       # 仮でユーザーIDを１にしている
-      user_id: 1,
+      # user_id: 1,
+      user_id: current_user.id,
       status_id: @status
     ) 
   end
