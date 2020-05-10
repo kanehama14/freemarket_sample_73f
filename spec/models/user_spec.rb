e70cc8038695780ffe3e43a59f0f06f752808eab
yukino0307 <62429496+yukino0307@users.noreply.github.com>
2020-05-10 12:00:45 +0900

    エラーメッセージの日本語化を加筆修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 describe User do
+  
   describe '#create' do
-    it "nameがない場合は登録できないこと" do
-      user = FactoryBot.build(:user, name: "")
-      user.valid?
-      expect(user.errors[:name]).to include("を入力してください")
-    end
+
+    #it "全ての必須項目が入力されている場合は登録できる" do
+    #  user = FactoryBot.build(:user)
+    #  expect(user).to be_valid
+    #end
+#
+    #it "nameがない場合は登録できないこと" do
+    #  user = FactoryBot.build(:user, name: "")
+    #  user.valid?
+    #  expect(user.errors[:name]).to include("が入力されていません。")
+    #end
+#
+    #it "emailは一意でない場合登録できないこと" do
+    #  user = FactoryBot.create(:user)
+    #      another_user = build(:user, email: user.email)
+    #      another_user.valid?
+    #      expect(another_user.errors[:email]).to include("は既に使用されています")
+    #end
     
     #it "全ての必須項目が入力されている場合出品できる" do
     #  item = FactoryBot.build(:item)
