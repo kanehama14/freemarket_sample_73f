a8eaa7ec3f3df82f84b7c9144f90fc10bb742cab
MasakiMimura <mimu36projectd@gmail.com>
2020-05-10 13:26:59 +0900

    card_function

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,8 +4,7 @@ class CardsController < ApplicationController
 
   def new
     @card = Card.new
-    # card = Card.where(user_id: current_user.id)
-    card = Card.where(user_id: 1)
+    card = Card.where(user_id: current_user.id)
     if card.exists?
       redirect_to card_path(card[0].id)
     end
@@ -14,23 +13,27 @@ class CardsController < ApplicationController
   def create
     # 秘密鍵を設定
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-    # トークンを作成 
-    token = Payjp::Token.create({
-      card: {
-        number:     params[:card][:number],
-        cvc:        params[:card][:cvc],
-        exp_month:  params[:card][:exp_month],
-        exp_year:   params[:card][:exp_year]
-      }},
-      {'X-Payjp-Direct-Token-Generate': 'true'} 
-    )
+    begin
+      # トークンを作成 
+      token = Payjp::Token.create({
+        card: {
+          number:     params[:card][:number],
+          cvc:        params[:card][:cvc],
+          exp_month:  params[:card][:exp_month],
+          exp_year:   params[:card][:exp_year]
+        }},
+        {'X-Payjp-Direct-Token-Generate': 'true'} 
+      )
+    rescue Payjp::CardError => e
+      @message = "このカードはご利用になれません。お手数ですが、窓口までお問い合わせください。"
+      redirect_to new_card_path, alert: @message
+    end
     if token.blank?
-      redirect_to new_card_path
+      # redirect_to new_card_path
     else
       # 上記で作成したトークンをもとに顧客情報を作成
       customer = Payjp::Customer.create(card: token)
-      # @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      card = Card.new(user_id: 1, customer_id: customer.id, card_id: customer.default_card)
+      card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if card.save!
         redirect_to card_path(card)
       else
@@ -40,8 +43,7 @@ class CardsController < ApplicationController
   end
 
   def destroy #PayjpとCardデータベースを削除
-    # card = Card.where(user_id: current_user.id).first
-    card = Card.where(user_id: 1).first
+    card = Card.where(user_id: current_user.id).first
     if card.blank?
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
@@ -53,16 +55,14 @@ class CardsController < ApplicationController
   end
 
   def show
-    # show画面にテストデータを入れる
-    # @default_card_information = {number: "4242424242424242", exp_month: "12", exp_year: "2020"}
-    # card = Card.find_by(user_id: current_user.id)
-    card = Card.find_by(user_id: 1)
-    if card.blank?
+    # @card1 = Card.find_by(user_id: current_user.id, id: params[:id])
+    @card1 = Card.find_by(user_id: current_user.id)
+    if @card1.blank?
       redirect_to action: "create"
     else
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @card = Payjp::Customer.retrieve(card.customer_id).cards.data[0]
+      customer = Payjp::Customer.retrieve(@card1.customer_id)
+      @card = Payjp::Customer.retrieve(@card1.customer_id).cards.data[0]
     end
   end
 end
