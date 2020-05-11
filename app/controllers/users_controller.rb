d2419b5b557da0bc5b0bbf0b78921b3b65f69e67
MasakiMimura <mimu36projectd@gmail.com>
2020-05-11 19:10:48 +0900

    payjp-fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   # require "payjp"
   def credit_regist
   #   card = Card.find_by(user_id: 1)
-  #   Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+  #   Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
   #   customer = Payjp::Customer.retrieve(card.customer_id)
   #   @card = Payjp::Customer.retrieve(card.customer_id).cards.data[0]
   end
