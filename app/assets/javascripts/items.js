be7b09a2a6a4c7f956634cfdddf17c4a2f055b7d
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-05-27 17:39:35 +0900

    [WIP]削除ボタンをクリック後の処理
    削除ボタンをクリックして初期表示されている添付エリアを非表示にする

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,6 +1,7 @@
 $(document).on('turbolinks:load', ()=> {
+$(document).on('turbolinks:load', () => {
   // 画像用のinputを生成する関数
-  const buildFileField = (index)=> {
+  const buildFileField = (index) => {
     const html = `<label class="js-file_group" data-index="${index}">
                     <i class="fa fa-camera clkarea-icon"></i>
                     <input class="js-file" type="file"
@@ -8,10 +9,10 @@ $(document).on('turbolinks:load', ()=> {
                     id="item_images_attributes_${index}_src">
                   </label>`;
 
-                  return html;
+    return html;
   }
   // プレビュー用のimgタグを生成する関数
-  const buildImg = (index, url)=> {
+  const buildImg = (index, url) => {
     const html = `<div class="prepic" data-index="${index}">
                     <img data-index="${index}" class="tmpimg" src="${url}">
                     <br>
@@ -23,7 +24,7 @@ $(document).on('turbolinks:load', ()=> {
     return html;
   }
   // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  let fileIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   // 既に使われているindexを除外
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
@@ -31,15 +32,15 @@ $(document).on('turbolinks:load', ()=> {
   // $(`div[data-index="10"]`).remove();
   $(`label[new="new"]`).css('width', `${120 * 5}`);
   // $(`div[edit="new"]`).css('width', `${120 * 5}`);
-  if(fileIndex[0]<=5){
-    $(`div[data-index="${fileIndex[0]-1}"]`).css('width', `${120 * (6 - fileIndex[0])}`);
-  }else if(fileIndex[0]<=10){
-    $(`div[data-index="${fileIndex[0]-1}"]`).css('width', `${120 * (11 - fileIndex[0])}`);
-  }else{
+  if (fileIndex[0] <= 5) {
+    $(`div[data-index="${fileIndex[0] - 1}"]`).css('width', `${120 * (6 - fileIndex[0])}`);
+  } else if (fileIndex[0] <= 10) {
+    $(`div[data-index="${fileIndex[0] - 1}"]`).css('width', `${120 * (11 - fileIndex[0])}`);
+  } else {
     $(`div[data-index="10"]`).remove();
   }
 
-  $('#image-box').on('change', '.js-file', function(e) {
+  $('#image-box').on('change', '.js-file', function (e) {
     const targetIndex = $(this).parent().data('index');
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
@@ -49,7 +50,7 @@ $(document).on('turbolinks:load', ()=> {
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('image', blobUrl);
-    } else {  
+    } else {
       // 新規画像追加の処理
       $('#previews').append(buildImg(targetIndex, blobUrl));
       // fileIndexの先頭の数字を使ってinputを作る
@@ -63,27 +64,27 @@ $(document).on('turbolinks:load', ()=> {
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
 
       // 画像追加して呼び出された添付ボタンを非表示
-      $(`input[name="item[images_attributes][${fileIndex[0]-2}][image]"]`).addClass("hidden-field");
+      $(`input[name="item[images_attributes][${fileIndex[0] - 2}][image]"]`).addClass("hidden-field");
       $(`div[edit="new"]`).addClass("hidden-field");
-      $(`label[data-index="${fileIndex[0]-2}"]`).addClass("hidden-field");
+      $(`label[data-index="${fileIndex[0] - 2}"]`).addClass("hidden-field");
       $('.hidden-destroy').hide();
 
       // $(`label[data-index="10"]`).remove();
-      if(fileIndex[0]<=5){
-        $(`label[data-index="${fileIndex[0]-1}"]`).css('width', `${120 * (6 - fileIndex[0])}`);
-      }else if(fileIndex[0]<=10){
-        $(`label[data-index="${fileIndex[0]-1}"]`).css('width', `${120 * (11 - fileIndex[0])}`);
-      }else{
+      if (fileIndex[0] <= 5) {
+        $(`label[data-index="${fileIndex[0] - 1}"]`).css('width', `${120 * (6 - fileIndex[0])}`);
+      } else if (fileIndex[0] <= 10) {
+        $(`label[data-index="${fileIndex[0] - 1}"]`).css('width', `${120 * (11 - fileIndex[0])}`);
+      } else {
         $(`label[data-index="10"]`).remove();
       }
-      
+
       // $(`label[data-index="5"]`).css('width', '100%');
 
       // console.log(fileIndex[0]);
     }
   });
 
-  $('#image-box').on('click', '.js-remove', function() {
+  $('#image-box').on('click', '.js-remove', function () {
     const targetIndex = $(this).parent().data('index');
     // 該当indexを振られているチェックボックスを取得する
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
@@ -94,7 +95,7 @@ $(document).on('turbolinks:load', ()=> {
     // $(this).remove();
     // $(`img[data-index="${targetIndex}"]`).remove();
     // $(`div class="js-file_group" data-index="${targetIndex}"`).remove();
-    
+
     // fileIndex.shift();
     // console.log(fileIndex);
     fileIndex.push(fileIndex[fileIndex.length - 1]-1);
@@ -114,7 +115,7 @@ $(document).on('turbolinks:load', ()=> {
 
 
 // jquery書きはじめ
-$(function (){
+$(function () {
   // 処理（ページが読み込まれた時、フォームに残り何文字入力できるかを数えて表示する）
 
   //フォームに入力されている文字数を数える
@@ -124,22 +125,22 @@ $(function (){
   var now_count = 1000 - count;
   //文字数がオーバーしていたら文字色を赤にする
   if (count > 1000) {
-    $(".js-text-count").css("color","red");
+    $(".js-text-count").css("color", "red");
   }
   //残りの入力できる文字数を表示
   $(".js-text-count").text(now_count + "/1000");
 
-  $(".js-text").on("keyup", function() {
+  $(".js-text").on("keyup", function () {
     // 処理（キーボードを押した時、フォームに残り何文字入力できるかを数えて表示する）
     //フォームのvalueの文字数を数える
     var count = $(this).val().replace(/\n/g, "改行").length;
     var now_count = 1000 - count;
 
     if (count > 1000) {
-      $(".js-text-count").css("color","red");
+      $(".js-text-count").css("color", "red");
     } else {
-      $(".js-text-count").css("color","black");
+      $(".js-text-count").css("color", "black");
     }
-    $(".js-text-count").text( now_count + "/1000");
+    $(".js-text-count").text(now_count + "/1000");
   });
 });
